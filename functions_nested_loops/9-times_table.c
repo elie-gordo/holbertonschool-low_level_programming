b45b2c35d77e5fdebc5e793a2c88800f435cdd72
elie-gordo <elliott.lanza>
2026-02-24 16:10:36 +0000

    feat(functions_nested_loops): add times_table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int row;
+	int col;
+	int result;
+
+	row = 0;
+	while (row <= 9)
+	{
+		col = 0;
+		while (col <= 9)
+		{
+			result = row * col;
+			if (col == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+			}
+			col++;
+		}
+		_putchar('\n');
+		row++;
+	}
+}
