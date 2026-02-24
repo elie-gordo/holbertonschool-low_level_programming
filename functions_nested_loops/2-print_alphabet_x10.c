9f183a2388f78e07b6a50ec7dcd010b3b9bea495
elie-gordo <elliott.lanza>
2026-02-24 15:48:53 +0000

    feat(functions_nested_loops): add print_alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet ten times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int line;
+	char c;
+
+	line = 0;
+	while (line < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		line++;
+	}
+}
