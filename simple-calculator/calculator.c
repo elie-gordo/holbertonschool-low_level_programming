8f452de9aa86ecdde9f75f227199343815a1d2a4
elie-gordo <elliott-lanza@gmail.com>
2026-03-02 12:58:22 +0000

    Simplify calculator code to strict minimal

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,37 +1,24 @@
 #include <stdio.h>
 
 /**
- * print_menu - displays the calculator menu
- *
- * Return: nothing
- */
-void print_menu(void)
-{
-	printf("Simple Calculator\n");
-	printf("1) Add\n");
-	printf("2) Subtract\n");
-	printf("3) Multiply\n");
-	printf("4) Divide\n");
-	printf("0) Quit\n");
-	printf("Choice: ");
-}
-
-/**
  * main - program entry point
  *
- * Return: 0 on success, 1 if input cannot be read
+ * Return: Always 0
  */
 int main(void)
 {
 	int choice;
-	int read_status;
 
 	while (1)
 	{
-		print_menu();
-		read_status = scanf("%d", &choice);
-		if (read_status != 1)
-			return (1);
+		printf("Simple Calculator\n");
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
+		printf("Choice: ");
+		scanf("%d", &choice);
 		if (choice == 0)
 		{
 			printf("Bye!\n");
