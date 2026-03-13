a507ddf4ef9915dcfa9b35939ccb3501c2ac1ba5
Elie <elliott.lanza@gmail.com>
2026-03-13 07:40:29 +0100

    Fix NULL handling in str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,20 +19,24 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	len2 = 0;
 
-	if (s1 != NULL)
+	if (s1 == NULL)
 	{
-		while (s1[len1] != '\0')
-		{
-			len1++;
-		}
+		s1 = "";
 	}
 
-	if (s2 != NULL)
+	if (s2 == NULL)
 	{
-		while (s2[len2] != '\0')
-		{
-			len2++;
-		}
+		s2 = "";
+	}
+
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (s2[len2] != '\0')
+	{
+		len2++;
 	}
 
 	concat = malloc(sizeof(char) * (len1 + len2 + 1));
