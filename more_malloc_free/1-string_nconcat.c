dcd61f46a465c642eac5e1bf96e857adb2ef50b6
Elie <elliott.lanza@gmail.com>
2026-03-18 08:58:26 +0000

    Fix Betty warning in string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,56 +12,31 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	unsigned int i;
-	unsigned int j;
-	unsigned int len1;
-	unsigned int len2;
-
-	len1 = 0;
-	len2 = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
-
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
-
-	if (n >= len2)
-	{
+	if (n > len2)
 		n = len2;
-	}
-
 	concat = malloc(sizeof(char) * (len1 + n + 1));
 	if (concat == NULL)
-	{
 		return (NULL);
-	}
-
-	for (i = 0; i < len1; i++)
+	while (i < len1)
 	{
 		concat[i] = s1[i];
+		i++;
 	}
-
-	for (j = 0; j < n; j++)
+	while (j < n)
 	{
 		concat[i + j] = s2[j];
+		j++;
 	}
-
 	concat[i + j] = '\0';
-
 	return (concat);
 }
