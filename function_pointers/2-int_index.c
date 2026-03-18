f8b4a412298ee2d4d982a509e0ccb4d7428371b2
Elie <elliott.lanza@gmail.com>
2026-03-18 11:58:31 +0000

    Add int_index and update function_pointers docs

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: input array
+ * @size: number of elements in array
+ * @cmp: comparison function
+ *
+ * Return: index of first match, or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == 0 || cmp == 0 || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
--- function_pointers/README.md
+++ function_pointers/README.md
@@ -22,3 +22,4 @@ Ce projet introduit l'utilisation des pointeurs de fonctions en C.
 - `function_pointers.h`
 - `0-print_name.c`
 - `1-array_iterator.c`
+- `2-int_index.c`
